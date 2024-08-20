cf36f8064d9c1ebdd7c5ca45778bca76f4f2b0da
KrishM-codes <krishmoorjani04@gmail.com>
2024-08-20 16:38:16 +0530

    Update Navbar.js
    Added Profile details

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,6 +2,13 @@ import React from "react";
 import { Link } from "react-router-dom";
 
 export default function Navbar() {
+  // Retrieve user data from localStorage
+  const email = localStorage.getItem("email");
+  const userId = localStorage.getItem("userid");
+  const username = localStorage.getItem("username");
+  const firstname = localStorage.getItem("firstname");
+  const lastname = localStorage.getItem("lastname");
+
   return (
     <div className="Navbar">
       <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
@@ -32,7 +39,6 @@ export default function Navbar() {
                   Complain/Query
                 </Link>
               </li>
-              
               <li className="nav-item">
                 <Link className="nav-link active" to="/complainttracker">
                   Complaint Tracker
@@ -44,37 +50,53 @@ export default function Navbar() {
                 </Link>
               </li>
             </ul>
-            <li className="nav-item dropdown dropstart" style={{color: 'white', display: 'flex', justifyContent: 'center', alignItems: 'center', paddingRight:'10px '}}>
-                <a
-                  className="nav-link dropdown-toggle"
-                  href="/"
-                  role="button"
-                  data-bs-toggle="dropdown"
-                  aria-expanded="false"
-                >
-                  Profile
-                </a>
-                <ul className="dropdown-menu">
-                  <li>
-                    <a className="dropdown-item" href="/">
-                      UserID
-                    </a>
-                  </li>
-                  <li>
-                    <a className="dropdown-item" href="/">
-                      Username
-                    </a>
-                  </li>
-                  <li>
-                    <hr className="dropdown-divider" />
-                  </li>
-                  <li>
-                    <a className="dropdown-item" href="/">
-                      Something else here
-                    </a>
-                  </li>
-                </ul>
-              </li>
+            <li
+              className="nav-item dropdown dropstart"
+              style={{
+                color: "white",
+                display: "flex",
+                justifyContent: "center",
+                alignItems: "center",
+                paddingRight: "10px ",
+              }}
+            >
+              <a
+                className="nav-link dropdown-toggle"
+                href="/"
+                role="button"
+                data-bs-toggle="dropdown"
+                aria-expanded="false"
+              >
+                Profile
+              </a>
+              <ul className="dropdown-menu">
+                <li>
+                  <a className="dropdown-item" href="/">
+                    {userId ? `User ID: ${userId}` : "User ID"}
+                  </a>
+                </li>
+                <li>
+                  <a className="dropdown-item" href="/">
+                    {username ? `Username: ${username}` : "Username"}
+                  </a>
+                </li>
+                <li>
+                  <hr className="dropdown-divider" />
+                </li>
+                <li>
+                  <a className="dropdown-item" href="/">
+                    {firstname && lastname
+                      ? `Name: ${firstname} ${lastname}`
+                      : "Name"}
+                  </a>
+                </li>
+                <li>
+                  <a className="dropdown-item" href="/">
+                    {email ? `Email: ${email}` : "Email"}
+                  </a>
+                </li>
+              </ul>
+            </li>
           </div>
         </div>
       </nav>
